980561f23ebeb131c7264fa4f1ac885ebbcf50cd
gamersi <sigamer805@gmail.com>
2023-06-21 18:59:01 +0200

    Optimisation, added touch support, bugs and freezes fixed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var b;
-var f;
-var p;
+let b,
+    p,
+    f;
 
-function randomIntBetween(min, max) { 
+function randomIntBetween(min, max) {
     return Math.random() * (max - min + 1) + min
 }
-  
+
 
 function Ball() {
     this.ballX = 0;
@@ -13,27 +13,33 @@ function Ball() {
     this.velocityX = -1;
     this.velocityY = 1;
     this.width = 20;
-    this.velMin = -1.5;
+    this.halfWidth = this.width / 2;
+    this.velMin = -1.3;
     this.velMax = 1.3;
     this.height = this.width;
     this.ballElem = document.getElementById("ball");
     this.initBall = () => {
+        this.velocityX = -1;
+        this.velocityY = 1;
         this.ballElem.style.width = `${this.width}px`;
         this.ballX = (f.width / 2);
         this.ballY = (f.height / 2);
-        this.velocityX = this.velocityX * randomIntBetween(this.velMin,this.velMax)
-        this.velocityY = this.velocityY * randomIntBetween(this.velMin,this.velMax)
-        while(this.velocityX >= -0.5 && this.velocityX <= 0.5){
-            this.velocityX = this.velocityX * randomIntBetween(this.velMin,this.velMax)
+        this.velocityX = this.velocityX * randomIntBetween(this.velMin, this.velMax)
+        this.velocityY = this.velocityY * randomIntBetween(this.velMin, this.velMax)
+
+        while (this.velocityX >= -0.5 && this.velocityX <= 0.5) {
+            this.velocityX = -1 * randomIntBetween(this.velMin, this.velMax)
         }
-        while(this.velocityY >= -0.5 && this.velocityY <= 0.5) {
-            this.velocityY = this.velocityY * randomIntBetween(this.velMin,this.velMax)
+
+        while (this.velocityY >= -0.5 && this.velocityY <= 0.5) {
+            this.velocityY = 1 * randomIntBetween(this.velMin, this.velMax)
         }
+
         this.updatePositions();
     }
     this.updatePositions = () => {
-        this.ballElem.style.top = `${this.ballY - (this.width / 2)}px`;
-        this.ballElem.style.left = `${this.ballX - (this.width / 2)}px`;
+        this.ballElem.style.top = `${this.ballY - this.halfWidth}px`;
+        this.ballElem.style.left = `${this.ballX - this.halfWidth}px`;
     }
 }
 
@@ -43,135 +49,183 @@ function Pedals() {
     this.leftY = 5;
     this.rightY = 5;
     this.height = 100;
+    this.halfHeight = this.height / 2;
     this.width = 10;
     this.scoreLeft = 0;
     this.pedalSpeed = 1;
-    // this.AIImperfecion = 1;
-    this.moveInterval = [];
+    // if the screen height is higher than the width, the pedal speed is increased
+    if (f.height > f.width) {
+        this.pedalSpeed = 2;
+    }
     this.scoreRight = 0;
     this.initPedals = () => {
         this.leftY = f.height / 2;
         this.rightY = f.height / 2;
         this.updatePositions();
-        document.addEventListener("keydown", this.keyDown)
-        document.addEventListener("keyup", this.keyUp)
-    }
-    this.stopInterval = (interval) => {
-        clearInterval(this.moveInterval[interval]);
-        this.moveInterval.splice(interval, 1)
-    }
-    this.keyDown = (e) => {
-        if(e.code == "ArrowUp"){
-            this.moveInterval.push(setInterval(() => {
-                console.log("set interval" + this.moveInterval);
-                if((this.leftY - (this.height / 2)) > 0) this.leftY -= this.pedalSpeed;
-                if(this.moveInterval.length > 1){
-                    this.stopInterval(0)
-                }
-            }, 1000 / 500));
-        } else if(e.code == "ArrowDown") {
-            this.moveInterval.push(setInterval(() => {
-                console.log("set interval" + this.moveInterval);
-                if((this.leftY + (this.height / 2)) < f.height) this.leftY += this.pedalSpeed;
-                if(this.moveInterval.length > 1){
-                    this.stopInterval(0)
-                }
-            }, 1000 / 500));
-        }
-    }
-    this.keyUp = (e) => {
-        console.log("Keyup" + this.moveInterval.length)
-        if(this.moveInterval.length > 0){
-            if(this.moveInterval.length > 1) return console.error("RIP")
-            clearInterval(this.moveInterval[0])
-            console.log("cleared Interval" + this.moveInterval)
-            this.moveInterval.splice(0, 1)
-        } else {
-            console.error("Requested KeyUp but key was never down")
-        }
     }
     this.updatePositions = () => {
-        this.pedalLeftElem.style.top = `${this.leftY - (this.height / 2)}px`;
-        this.pedalRightElem.style.top = `${this.rightY - (this.height / 2)}px`;
+        this.pedalLeftElem.style.top = `${this.leftY - (this.halfHeight)}px`;
+        this.pedalRightElem.style.top = `${this.rightY - (this.halfHeight)}px`;
     }
 }
 
 function Field() {
     this.fieldElem = document.getElementById("field");
     this.scoreElem = document.getElementById("score");
-    this.fpsElem = document.getElementById("fps");
     this.width = this.fieldElem.offsetWidth;
     this.height = this.fieldElem.offsetHeight;
+    this.animationFrameId = null;
+    this.pressedKeys = [];
+    this.opAI = false;
+    this.lastTapTime = 0;
+    this.doubleTapSpeed = 300;
+
     this.initGame = () => {
-        this.isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-        if(this.isFirefox) alert("!!!: Firefox + Early ALPHA = Probably many bugs ")
         b = new Ball();
         b.initBall();
         p = new Pedals();
         p.initPedals();
+        if (localStorage.getItem("opAI") === "true") {
+            this.opAI = true;
+        }
         window.addEventListener("resize", this.resize)
-        this.then = Date.now() / 1000;
-        this.updateInterval = setInterval(this.update, 1000 / 500);
+        this.animationFrameId = requestAnimationFrame(this.update);
+        window.addEventListener("keydown", this.keyDown);
+        window.addEventListener("keyup", this.keyUp);
+        // touchscreen support
+        window.addEventListener("touchstart", this.touchStart);
+        window.addEventListener("touchmove", this.touchMove);
+        window.addEventListener("touchend", this.touchEnd);
+    }
+    this.keyDown = (e) => {
+        this.pressedKeys[e.code] = true;
+        if (e.code === "Escape" && !this.isPaused) {
+            this.pauseGame();
+            this.isPaused = true;
+        }
+        else if (e.code === "Escape" && this.isPaused) {
+            this.resumeGame();
+            this.isPaused = false;
+        }
+    }
+    this.keyUp = (e) => {
+        this.pressedKeys[e.code] = false;
+    }
+    this.touchStart = (e) => {
+        if (e.touches.length > 0) {
+            if (e.touches[0].clientY <= this.height / 2) {
+                this.pressedKeys["ArrowUp"] = true;
+            } else {
+                this.pressedKeys["ArrowDown"] = true;
+            }
+        }
+    }
+    this.touchMove = (e) => {
+        if (e.touches.length > 0) {
+            if (e.touches[0].clientY <= this.height / 2) {
+                this.pressedKeys["ArrowUp"] = true;
+                this.pressedKeys["ArrowDown"] = false;
+            } else {
+                this.pressedKeys["ArrowDown"] = true;
+                this.pressedKeys["ArrowUp"] = false;
+            }
+        }
+    }
+    this.touchEnd = (e) => {
+        if (e.changedTouches.length > 0) {
+            if (e.changedTouches[0].clientY <= this.height / 2) {
+                this.pressedKeys["ArrowUp"] = false;
+            } else {
+                this.pressedKeys["ArrowDown"] = false;
+            }
+        } else {
+            console.error("congrats, you broke it again");
+            this.pressedKeys["ArrowUp"] = false;
+            this.pressedKeys["ArrowDown"] = false;
+        }
     }
     this.resize = (e) => {
-        // b.initBall();
-        // p.initPedals();
         this.width = this.fieldElem.offsetWidth;
         this.height = this.fieldElem.offsetHeight;
+        if (this.height > this.width) {
+            p.pedalSpeed = 2;
+        } else {
+            p.pedalSpeed = 1;
+        }
+    }
+    this.pauseGame = () => {
+        cancelAnimationFrame(this.animationFrameId);
+    }
+    this.resumeGame = () => {
+        console.log("resume");
+        this.animationFrameId = requestAnimationFrame(this.update);
+    }
+    this.resetGame = () => {
+        this.pauseGame();
+        b.initBall();
+        p.initPedals();
+        this.animationFrameId = requestAnimationFrame(this.update);
     }
     this.update = () => {
         b.ballX += b.velocityX;
         b.ballY += b.velocityY;
+
+        // Collisions
         //bounce
-        if (b.ballY  >= this.height - (b.width / 2) || b.ballY <= (b.width / 2)) {
+        if (b.ballY >= this.height - (b.width / 2) || b.ballY <= (b.width / 2)) {
             b.velocityY = b.velocityY * (-1);
         }
         //pedals
         //left
-        if(b.ballY >= p.leftY - (p.height / 2) && b.ballY <= p.leftY + (p.height / 2)
-            && b.ballX <= 25){
-                b.velocityX = b.velocityX * (-1);
+        if (b.ballY >= p.leftY - (p.height / 2) && b.ballY <= p.leftY + (p.height / 2)
+            && b.ballX <= 25) {
+            b.velocityX = b.velocityX * (-1);
         }
         //right
-        if(b.ballY >= p.rightY - (p.height / 2) && b.ballY <= p.rightY + (p.height / 2)
-            && b.ballX >= this.width-25){
-                b.velocityX = b.velocityX * (-1);
+        if (b.ballY >= p.rightY - (p.height / 2) && b.ballY <= p.rightY + (p.height / 2)
+            && b.ballX >= this.width - 25) {
+            b.velocityX = b.velocityX * (-1);
         }
 
-
-        //right pedal AI
-        if(b.ballY < p.rightY && (p.rightY - (p.height / 2)) > 0){
-            p.rightY -= p.pedalSpeed /*+ p.AIImperfecion*/
+        // left pedal position update
+        if (this.pressedKeys["ArrowUp"] && p.leftY - p.height / 2 > 0) {
+            p.leftY -= p.pedalSpeed;
         }
 
-        if(b.ballY > p.rightY && (p.rightY + (p.height / 2)) < this.height) {
-            p.rightY += p.pedalSpeed /*+ p.AIImperfecion*/
+        if (this.pressedKeys["ArrowDown"] && p.leftY + p.height / 2 < f.height) {
+            p.leftY += p.pedalSpeed;
+        }
+
+        // right pedal AI
+        if (!this.opAI) {
+            if (b.ballY < p.rightY && p.rightY - p.height / 2 > 0 && !this.opAI) {
+                p.rightY -= p.pedalSpeed;
+            }
+
+            if (b.ballY > p.rightY && p.rightY + p.height / 2 < f.height && !this.opAI) {
+                p.rightY += p.pedalSpeed;
+            }
+        } else {
+            p.rightY = Math.max(Math.min(b.ballY, f.height - p.height / 2), p.height / 2);
+
         }
 
         //lose
         if (b.ballX <= (b.width / 2) || b.ballX >= this.width - (b.width / 2)) {
-            if(b.ballX <= (b.width / 2)){
-                p.scoreLeft += 1;
-                this.scoreElem.textContent = `${p.scoreLeft}|${p.scoreRight}`
-            }
-            if(b.ballX >= this.width - (b.width / 2)) {
+            if (b.ballX <= (b.width / 2)) {
                 p.scoreRight += 1;
-                this.scoreElem.textContent = `${p.scoreLeft}|${p.scoreRight}`
             }
-            b.initBall();
-            p.initPedals();
+            if (b.ballX >= this.width - (b.width / 2)) {
+                p.scoreLeft += 1;
+            }
+            this.scoreElem.textContent = `${p.scoreLeft}|${p.scoreRight}`;
+            return this.resetGame();
         }
+
         b.updatePositions();
         p.updatePositions();
-        
-        //fps
-        this.now = Date.now() / 1000;  
 
-        this.elapsedTime = this.now - this.then;
-        this.then = this.now;
-        
-        this.fps = 1 / this.elapsedTime;
-        this.fpsElem.innerText = this.fps.toFixed(2); 
+        this.animationFrameId = requestAnimationFrame(this.update);
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,18 @@
+# JSPong
+A simple pong game made in JavaScript, HTML and CSS.
+Originally coded in 2-3 hours out of boredom in 2021, but I had to optimize it a bit and fix some bugs so that it is playable.
+## How to play
+You can play the game [here](https://jspong.gamersi.at/). You can also download the files and open the index.html file in your browser.
+## Controls
+arrow up: move paddle up
+arrow down: move paddle down
+on mobile: touch the screen and depending on where you touch, the paddle will move up or down. if you touch the upper half of the screen, the paddle will move up. if you touch the lower half of the screen, the paddle will move down.
+The right paddle is controlled by an AI. you can make it harder by changing "opAI" in localStorage to "true".
+## Contributing
+If you want to contribute to this project, feel free to do so. I will look at your pull request and decide if I want to merge it.
+## License
+This project is licensed under the MIT license. You can find it in the LICENSE file.
+## Credits
+- [gamersi](https://www.github.com/gamersi) - Main developer
+- [critiqalfish](https://www.github.com/critiqalfish) - Helped with optimizing the code
+- [MDN Web Docs](https://developer.mozilla.org) - Helped with the touch controls
